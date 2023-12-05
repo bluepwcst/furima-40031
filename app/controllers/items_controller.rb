7c96749190a43830c037687c27cba9b412993ac0
bluepwcst <takatomo2000zzz@gmail.com>
2023-12-05 19:27:06 +0900

    レビュー後修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,32 +1,26 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
   end
 
   def show
-    @item = Item.find(params[:id])
   end
   
   def edit
-    @item = Item.find_by(id: params[:id])
     # アイテムが見つからない、または現在のユーザーがアイテムの所有者でない場合はトップページにリダイレクト
-    if @item.nil? || current_user.id != @item.user_id
-      redirect_to root_path
+    redirect_to root_path if @item.nil? || current_user.id != @item.user_id
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
-
-
-def update
-  @item = Item.find(params[:id])
-  if @item.update(item_params)
-    redirect_to item_path(@item)
-  else
-    render :edit, status: :unprocessable_entity
-  end
-end
-
 
   def new
     @item = Item.new
@@ -42,6 +36,10 @@ end
   end
   
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :region_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
